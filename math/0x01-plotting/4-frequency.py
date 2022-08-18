ef032759ca3a97ab659a2777c6a34af9e9dd9d21
Cecales <cesar.caleroes@gmail.com>
2022-08-18 15:45:58 -0500

    Upload other points

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,4 +5,12 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-# your code here
+x_bins = np.arange(0, 110, 10)
+plt.hist(student_grades, bins=x_bins, edgecolor='black')
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.title("Project A")
+plt.xticks(x_bins)
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.show()
