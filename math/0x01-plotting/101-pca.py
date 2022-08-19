7dda5e60f2de255205ee59534fec9e3be7cd00de
Cecales <cesar.caleroes@gmail.com>
2022-08-18 21:31:06 -0500

    Upload last points

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -12,4 +12,16 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-# your code here
+colors = {
+        0: '#3800a5',
+        1: 'magenta',
+        2: '#e8f301'
+    }
+
+fig = plt.figure()
+ax.scatter(u1, u2, u3, c=colors.get(labels[i]), alpha=0.5)
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
+ax.set_title('PCA of Iris Dataset')
+plt.show()
